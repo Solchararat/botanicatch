8c5cca4db98e2be3e18bfef7c5a59b179d793ec0
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2025-04-26 16:19:06 +0800

    feat: Add custom auth button style

---
+++ lib/widgets/buttons/auth_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:botanicatch/utils/constants.dart';
+
+class AuthButton extends StatefulWidget {
+  final String title;
+  final VoidCallback onPressed;
+
+  const AuthButton({super.key, required this.title, required this.onPressed});
+
+  @override
+  State<AuthButton> createState() => _AuthButtonState();
+}
+
+class _AuthButtonState extends State<AuthButton> {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: widget.onPressed,
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.all(kGreenColor100),
+        shape: WidgetStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+            side: BorderSide(color: kGreenColor100),
+          ),
+        ),
+      ),
+      child: Text(
+        widget.title,
+        style: kSmallTextStyle.copyWith(
+            color: kGreenColor500, fontWeight: FontWeight.bold, fontSize: 16),
+      ),
+    );
+  }
+}
