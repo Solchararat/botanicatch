1d930d86e323048455f5e7e4c77d88eb24b43107
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2025-04-26 23:10:55 +0800

    feat: Add parent authenticate screen for toggling between login and register screens

---
+++ lib/screens/auth/authenticate.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:botanicatch/screens/auth/login_screen.dart';
+import 'package:botanicatch/screens/auth/register_screen.dart';
+
+class Authenticate extends StatefulWidget {
+  const Authenticate({super.key});
+
+  @override
+  State<Authenticate> createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  bool _showLoginScreen = true;
+
+  void _toggleView() => setState(() => _showLoginScreen = !_showLoginScreen);
+
+  @override
+  Widget build(BuildContext context) {
+    return _showLoginScreen
+        ? LoginScreen(toggleView: _toggleView)
+        : RegisterScreen();
+  }
+}
