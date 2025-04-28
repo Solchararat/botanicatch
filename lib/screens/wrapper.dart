afcc7e0f7946fe47a6ad4f32247bbc94ffed0a5f
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2025-04-28 16:51:52 +0800

    fix(ui): Fix resizing issues in auth screens

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -12,6 +12,7 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: kGreenColor500,
       body: Stack(
         children: [
