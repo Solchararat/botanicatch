a0b8ae1a08a156b9aafb2ffa8a101c47db7b12b2
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2025-04-28 13:16:14 +0800

    feat(auth): Implement sign out feature

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,13 +1,26 @@
+import 'package:botanicatch/services/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+  static final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("You are now logged in!"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("You are now logged in!"),
+            TextButton(
+                onPressed: () {
+                  _auth.signOut();
+                },
+                child: const Text("Log Out"))
+          ],
+        ),
       ),
     );
   }
