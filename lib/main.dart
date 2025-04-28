6ad52e20f8da638762a445fd8aeee4454c322758
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2025-04-28 11:00:39 +0800

    feat(ui): Implement streambuilder in wrapper to handle auth effectively

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 import 'package:botanicatch/models/user_model.dart';
 import 'package:botanicatch/screens/auth/start_screen.dart';
 import 'package:botanicatch/services/auth_service.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:botanicatch/firebase_options.dart';
-import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,11 +1,48 @@
 import 'package:botanicatch/screens/auth/authenticate.dart';
+import 'package:botanicatch/screens/auth/verify_email_screen.dart';
+import 'package:botanicatch/screens/home/home.dart';
+import 'package:botanicatch/services/auth_service.dart';
+import 'package:botanicatch/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
+  static final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return const Authenticate();
+    return Scaffold(
+      backgroundColor: kGreenColor500,
+      body: Stack(
+        children: [
+          StreamBuilder(
+              stream: _auth.authStateChanges,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: kGreenColor500,
+                      color: kGreenColor300,
+                    ),
+                  );
+                } else if (snapshot.hasError) {
+                  return const Center(
+                    child: Text("An error has occured."),
+                  );
+                } else {
+                  if (snapshot.data == null) {
+                    return const Authenticate();
+                  } else {
+                    if (snapshot.data!.emailVerified) {
+                      return const HomeScreen();
+                    } else {
+                      return VerifyEmailScreen(email: snapshot.data?.email);
+                    }
+                  }
+                }
+              })
+        ],
+      ),
+    );
   }
 }
