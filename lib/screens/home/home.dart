44ae859d282c833be7016db32a5da89ec4afaac6
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2025-04-29 12:51:16 +0800

    feat: Abstracted grid for modularity

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,6 +1,8 @@
 import 'package:botanicatch/utils/constants.dart';
 import 'package:botanicatch/widgets/background-image/background_image.dart';
+import 'package:botanicatch/widgets/buttons/camera_fab.dart';
 import 'package:botanicatch/widgets/buttons/plant_action_button.dart';
+import 'package:botanicatch/widgets/grids/plant_action_grid.dart';
 import 'package:flutter/material.dart';
 import 'package:botanicatch/services/auth_service.dart';
 import 'package:botanicatch/widgets/navbars/custom_bottom_navbar.dart';
@@ -45,10 +47,11 @@ class _HomeScreenState extends State<HomeScreen> {
             padding: const EdgeInsets.all(8.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     SizedBox(width: 45),
                     Image.asset(
@@ -59,92 +62,87 @@ class _HomeScreenState extends State<HomeScreen> {
                     Image.asset("assets/images/user.png")
                   ],
                 ),
-                Text("Hi Guest!",
-                    style:
-                        kLargeTextStyle.copyWith(fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.left),
-                Text("Anything New?",
-                    style: kSmallTextStyle.copyWith(color: kGrayColor250),
-                    textAlign: TextAlign.left),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("Hi Guest!",
+                      style:
+                          kLargeTextStyle.copyWith(fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.left),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("Anything New?",
+                      style: kSmallTextStyle.copyWith(color: kGrayColor250),
+                      textAlign: TextAlign.left),
+                ),
                 const SizedBox(height: 16),
+                PlantActionGrid(
+                  topLeftWidget: PlantActionButton(
+                    onTap: () {},
+                    imagePath: "assets/images/new-plants.png",
+                    title: "New Plants",
+                    // TODO: Implement onTap feature
+                    mainColor: kGreenColor300,
+                    radialColor: kGreenColor500,
+                    right: -40,
+                    top: -20,
+                    cacheWidth: 110,
+                    cacheHeight: 110,
+                  ),
+                  topRightWidget: PlantActionButton(
+                    // TODO: Implement onTap feature
+                    onTap: () {},
+                    imagePath: "assets/images/your-plants.png",
+                    title: "Your Plants",
+                    mainColor: kGreenColor300,
+                    radialColor: kGreenColor500,
+                    right: -35,
+                    top: -35,
+                    cacheWidth: 125,
+                    cacheHeight: 125,
+                  ),
+                  bottomLeftWidget: PlantActionButton(
+                    // TODO: Implement onTap feature
+                    onTap: () {},
+                    imagePath: "assets/images/location.png",
+                    title: "Location",
+                    mainColor: kBlueColor100,
+                    radialColor: kBlueColor200,
+                    right: -25,
+                    top: -25,
+                    cacheWidth: 100,
+                    cacheHeight: 100,
+                  ),
+                  bottomRightWidget: PlantActionButton(
+                    // TODO: Implement onTap feature
+                    onTap: () {},
+                    imagePath: "assets/images/settings.png",
+                    title: "Location",
+                    mainColor: kBlueColor100,
+                    radialColor: kBlueColor200,
+                    right: -15,
+                    top: -25,
+                    cacheWidth: 110,
+                    cacheHeight: 110,
+                  ),
+                ),
                 Column(
-                  spacing: 16,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        PlantActionButton(
-                          onTap: () {},
-                          imagePath: "assets/images/new-plants.png",
-                          title: "New Plants",
-                          // TODO: Implement onTap feature
-                          mainColor: kGreenColor300,
-                          radialColor: kGreenColor500,
-                          right: -40,
-                          top: -20,
-                          cacheWidth: 110,
-                          cacheHeight: 110,
-                        ),
-                        PlantActionButton(
-                          // TODO: Implement onTap feature
-                          onTap: () {},
-                          imagePath: "assets/images/your-plants.png",
-                          title: "Your Plants",
-                          mainColor: kGreenColor300,
-                          radialColor: kGreenColor500,
-                          right: -35,
-                          top: -35,
-                          cacheWidth: 125,
-                          cacheHeight: 125,
-                        ),
-                      ],
+                    Text(
+                      "You are now logged in!",
+                      style: kSmallTextStyle,
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        PlantActionButton(
-                          // TODO: Implement onTap feature
-                          onTap: () {},
-                          imagePath: "assets/images/location.png",
-                          title: "Location",
-                          mainColor: kBlueColor100,
-                          radialColor: kBlueColor200,
-                          right: -25,
-                          top: -25,
-                          cacheWidth: 100,
-                          cacheHeight: 100,
-                        ),
-                        PlantActionButton(
-                          // TODO: Implement onTap feature
-                          onTap: () {},
-                          imagePath: "assets/images/settings.png",
-                          title: "Location",
-                          mainColor: kBlueColor100,
-                          radialColor: kBlueColor200,
-                          right: -15,
-                          top: -25,
-                          cacheWidth: 110,
-                          cacheHeight: 110,
-                        )
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          "You are now logged in!",
-                          style: kSmallTextStyle,
-                        ),
-                        TextButton(
-                            onPressed: () {
-                              _auth.signOut();
-                            },
-                            child: Text(
-                              "Log Out",
-                              style: kSmallTextStyle.copyWith(
-                                  color: kGreenColor300),
-                            ))
-                      ],
-                    ),
+                    TextButton(
+                        onPressed: () {
+                          _auth.signOut();
+                        },
+                        child: Text(
+                          "Log Out",
+                          style:
+                              kSmallTextStyle.copyWith(color: kGreenColor300),
+                        ))
                   ],
                 ),
               ],
@@ -156,31 +154,8 @@ class _HomeScreenState extends State<HomeScreen> {
         selectedIndexNotifier: _currentScreenIndex,
         onDestinationSelect: (int index) => _navigateOnPress(index),
       ),
-      floatingActionButton: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            margin: const EdgeInsets.only(top: 50),
-            height: 64,
-            width: 64,
-            child: FloatingActionButton(
-              onPressed: () {},
-              elevation: 0,
-              backgroundColor: kGreenColor300,
-              shape: RoundedRectangleBorder(
-                  side: const BorderSide(width: 10, color: kGreenColor500),
-                  borderRadius: BorderRadius.circular(100)),
-              child: const Icon(
-                Icons.camera_alt_outlined,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          Text('Catch',
-              style: kXXSmallTextStyle.copyWith(
-                  fontSize: 11, color: kGrayColor250)),
-        ],
-      ),
+      // TODO: Implement onPress function
+      floatingActionButton: CameraFab(onPressed: () {}),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
