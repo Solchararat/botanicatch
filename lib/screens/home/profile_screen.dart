c5f90d85711d03c4223c0747dc54ddbe0bfe421b
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2025-04-30 11:57:20 +0800

    feat: Implement profile screen

--- lib/screens/home/profile_screen.dart
+++ lib/screens/home/profile_screen.dart
@@ -1,4 +1,6 @@
+import 'package:botanicatch/utils/constants.dart';
 import 'package:botanicatch/widgets/background-image/background_image.dart';
+import 'package:botanicatch/widgets/profile-picture/profile_picture.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -6,19 +8,42 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.transparent,
-      body: BackgroundImage(
-        imagePath: "assets/images/home_bg.jpg",
-        child: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                Text("Hello"),
-              ],
+      body: Stack(
+        children: [
+          BackgroundImage(
+            imagePath: "assets/images/home_bg.jpg",
+            child: SafeArea(
+              child: Center(
+                child: Column(
+                  children: [
+                    // accounts the size of the banner
+                    const SizedBox(height: 190),
+                    Text("Uncle Kevin", style: kSmallTextStyle),
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
+          // Photo by <a href="https://unsplash.com/@vladutremus?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Vladut Tomsa</a> on <a href="https://unsplash.com/photos/green-trees-on-green-grass-field-under-blue-sky-during-daytime-HrsJTdMqfvg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+          // TODO: Implement banner uploading if required.
+          Image.asset(
+            "assets/images/banner.jpg",
+            width: MediaQuery.of(context).size.width * 1,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Column(
+              children: [
+                const SizedBox(height: 140),
+                const ProfilePicture(),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
