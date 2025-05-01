9c7a6a9de88fbb590dbcc7dbee60066eb0f97462
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2025-05-01 22:50:22 +0800

    feat: Add activity item widget

---
+++ lib/widgets/profile/activity_item.dart
@@ -0,0 +1,18 @@
+import 'package:botanicatch/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+class ActivityItem extends StatelessWidget {
+  const ActivityItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 280,
+      height: 80,
+      decoration: BoxDecoration(
+        color: kGrayColor100,
+        borderRadius: BorderRadius.circular(20),
+      ),
+    );
+  }
+}
