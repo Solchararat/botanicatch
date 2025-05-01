a8c5d14054316e9f9f712804b570723d3ebdb7b4
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2025-05-01 22:49:56 +0800

    feat: Add hasPadding parameter

--- lib/widgets/background-image/background_image.dart
+++ lib/widgets/background-image/background_image.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 
 class BackgroundImage extends StatelessWidget {
-  final Widget child;
+  final Widget? child;
   final double? opacity;
   final String imagePath;
+  final bool? hasPadding;
+
   const BackgroundImage({
     super.key,
-    required this.child,
+    this.child,
     required this.imagePath,
+    this.hasPadding,
     this.opacity,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(16),
+      padding:
+          hasPadding != null && !hasPadding! ? null : const EdgeInsets.all(16),
       decoration: BoxDecoration(
         image: DecorationImage(
           image: AssetImage(imagePath),
