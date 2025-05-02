39177663f88101108e4bd4b277c4a3e149dd4a23
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2025-05-02 14:42:43 +0800

    feat: Add username parameter to user model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,8 +1,10 @@
 class UserModel {
   final String? uid;
   final String? email;
+  final String? username;
   const UserModel({
     required this.uid,
     required this.email,
+    this.username,
   });
 }
