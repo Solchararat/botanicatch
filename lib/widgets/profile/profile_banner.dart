1003ac2c5745f3f2c17fa53d8025290f03aa85ee
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2025-05-02 14:41:35 +0800

    feat: Implement profile banner widget

---
+++ lib/widgets/profile/profile_banner.dart
@@ -0,0 +1,141 @@
+import 'dart:typed_data';
+import 'dart:developer';
+import 'package:botanicatch/services/auth/auth_service.dart';
+import 'package:botanicatch/services/storage/storage_service.dart';
+import 'package:botanicatch/utils/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ProfileBanner extends StatefulWidget {
+  final bool isEditable;
+  const ProfileBanner({super.key, this.isEditable = false});
+
+  @override
+  State<ProfileBanner> createState() => _ProfileBannerState();
+}
+
+class _ProfileBannerState extends State<ProfileBanner> {
+  static final String? _uid = AuthService.instance.currentUser?.uid;
+  final StorageService _storage = StorageService.instance;
+  final ImagePicker _picker = ImagePicker();
+  final String _filePath = "users/$_uid/banner-picture/banner.jpg";
+  late final ValueNotifier<Uint8List?> _imageNotifier;
+
+  @override
+  void initState() {
+    super.initState();
+    _imageNotifier = ValueNotifier(null);
+    _getBannerPicture();
+  }
+
+  @override
+  void dispose() {
+    _imageNotifier.dispose();
+    super.dispose();
+  }
+
+  Future<void> _onBannerTapped() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image == null) return;
+    final imageBytes = await image.readAsBytes();
+    _imageNotifier.value = imageBytes;
+
+    await _storage.uploadFile(_filePath, image);
+  }
+
+  Future<void> _getBannerPicture() async {
+    try {
+      final imageBytes = await _storage.getFile(_filePath);
+      _imageNotifier.value = imageBytes;
+      if (imageBytes == null) return;
+    } catch (e) {
+      log("Banner picture not uploaded yet.");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.isEditable
+        ? Stack(
+            children: [
+              GestureDetector(
+                onTap: _onBannerTapped,
+                child: ValueListenableBuilder(
+                    valueListenable: _imageNotifier,
+                    builder: (_, imageBytes, __) {
+                      return Container(
+                        height: 100,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: imageBytes != null
+                              ? Colors.black
+                              : Colors.grey[300],
+                          image: imageBytes != null
+                              ? DecorationImage(
+                                  opacity: .5,
+                                  image: Image.memory(
+                                    imageBytes,
+                                    fit: BoxFit.cover,
+                                  ).image,
+                                  fit: BoxFit.cover)
+                              : null,
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.camera_alt_outlined,
+                                color: imageBytes != null
+                                    ? Colors.white
+                                    : Colors.black38,
+                                size: 30,
+                              ),
+                              Text(
+                                "Tap to change banner image",
+                                style: kXXSmallTextStyle.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 12,
+                                    color: imageBytes != null
+                                        ? Colors.white
+                                        : Colors.black38),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+            ],
+          )
+        : ValueListenableBuilder(
+            valueListenable: _imageNotifier,
+            builder: (_, imageBytes, __) {
+              return Container(
+                  height: 200,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    image: imageBytes != null
+                        ? DecorationImage(
+                            image: Image.memory(
+                              imageBytes,
+                              fit: BoxFit.cover,
+                            ).image,
+                            fit: BoxFit.cover)
+                        : null,
+                  ),
+                  child: imageBytes == null
+                      ? const Center(
+                          child: Icon(
+                            Icons.image,
+                            color: Colors.black38,
+                            size: 30,
+                          ),
+                        )
+                      : null);
+            });
+  }
+}
--- lib/widgets/profile/profile_header.dart
+++ lib/widgets/profile/profile_header.dart
@@ -1,3 +1,4 @@
+import 'package:botanicatch/widgets/profile/profile_banner.dart';
 import 'package:botanicatch/widgets/profile/profile_picture.dart';
 import 'package:flutter/material.dart';
 
@@ -11,10 +12,7 @@ class ProfileHeader extends StatelessWidget {
       // Photo by <a href="https://unsplash.com/@vladutremus?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Vladut Tomsa</a> on <a href="https://unsplash.com/photos/green-trees-on-green-grass-field-under-blue-sky-during-daytime-HrsJTdMqfvg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
       // TODO: Implement banner uploading if required.
       children: [
-        Image.asset("assets/images/banner.jpg",
-            width: MediaQuery.of(context).size.width * 1,
-            height: 200,
-            fit: BoxFit.cover),
+        const ProfileBanner(),
         Align(
           alignment: Alignment.topCenter,
           child: Column(
