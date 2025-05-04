bbafa4f21407f0cfe9a9bfe0d82c090b84a6c658
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2025-05-04 11:31:17 +0800

    chore: Move activity_item.dart to cards directory

--- lib/screens/home/profile_screen.dart
+++ lib/screens/home/profile_screen.dart
@@ -5,7 +5,7 @@ import 'package:botanicatch/utils/constants.dart';
 import 'package:botanicatch/widgets/background-image/background_image.dart';
 import 'package:botanicatch/widgets/modals/edit_profile_modal.dart';
 import 'package:botanicatch/widgets/profile/achievement_badges.dart';
-import 'package:botanicatch/widgets/profile/activity_item.dart';
+import 'package:botanicatch/widgets/cards/activity_item.dart';
 import 'package:botanicatch/widgets/profile/profile_header.dart';
 import 'package:botanicatch/widgets/profile/section_title.dart';
 import 'package:flutter/material.dart';
@@ -55,68 +55,64 @@ class _ProfileScreenState extends State<ProfileScreen> {
               ),
             ),
           )
-        : Scaffold(
-            resizeToAvoidBottomInset: false,
-            backgroundColor: Colors.transparent,
-            body: Stack(
-              children: [
-                // Background Image
-                BackgroundImage(
-                  imagePath: "assets/images/home_bg.jpg",
-                  hasPadding: false,
-                ),
+        : Stack(
+            children: [
+              // Background Image
+              BackgroundImage(
+                imagePath: "assets/images/home_bg.jpg",
+                hasPadding: false,
+              ),
 
-                SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    spacing: 16,
-                    children: [
-                      ProfileHeader(
-                        profileImgBytes: _profileImgBytes,
-                        bannerImgBytes: _bannerImgBytes,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        spacing: 8,
-                        children: [
-                          // to make the name centered
-                          const SizedBox(width: 16),
-                          Text(
-                            user.username ?? "null",
-                            style: kSmallTextStyle.copyWith(
-                              fontWeight: FontWeight.bold,
-                            ),
+              SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 16,
+                  children: [
+                    ProfileHeader(
+                      profileImgBytes: _profileImgBytes,
+                      bannerImgBytes: _bannerImgBytes,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      spacing: 8,
+                      children: [
+                        // to make the name centered
+                        const SizedBox(width: 16),
+                        Text(
+                          user.username ?? "null",
+                          style: kSmallTextStyle.copyWith(
+                            fontWeight: FontWeight.bold,
                           ),
-                          InkWell(
-                            onTap: () {
-                              showDialog(
-                                  context: context,
-                                  builder: (context) => EditProfileModal(
-                                        profileImgBytes: _profileImgBytes,
-                                        bannerImgBytes: _bannerImgBytes,
-                                      ));
-                            },
-                            child: const Icon(
-                              Icons.edit,
-                              color: Colors.white,
-                              size: 18,
-                            ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            showDialog(
+                                context: context,
+                                builder: (context) => EditProfileModal(
+                                      profileImgBytes: _profileImgBytes,
+                                      bannerImgBytes: _bannerImgBytes,
+                                    ));
+                          },
+                          child: const Icon(
+                            Icons.edit,
+                            color: Colors.white,
+                            size: 18,
                           ),
-                        ],
-                      ),
-                      const Divider(thickness: 0.5, height: 0.5),
-                      const AchievementBadges(),
-                      const Divider(thickness: 0.5, height: 0.5),
-                      ProfileSectionTitle(title: "Last Activities"),
-                      Column(
-                        children: _fetchLastActivities(),
-                      )
-                    ],
-                  ),
+                        ),
+                      ],
+                    ),
+                    const Divider(thickness: 0.5, height: 0.5),
+                    const AchievementBadges(),
+                    const Divider(thickness: 0.5, height: 0.5),
+                    ProfileSectionTitle(title: "Last Activities"),
+                    Column(
+                      children: _fetchLastActivities(),
+                    )
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           );
   }
 }
